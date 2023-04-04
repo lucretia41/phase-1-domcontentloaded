c060a1ff81c9237722334249f42c40d1ac81ab9e
Ian Scott Martin <ianscottmartin@gmail.com>
2023-04-04 16:57:25 -0600

    finit

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code goes here
+
+// document.addEventListener("DOMContentLoaded", function() {
+//   console.log("The DOM has loaded");
+// });
+
+// console.log(
+//   "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+// );
+// Code your solution in index.js. First, set up a DOMContentLoaded event listener to detect when the HTML page has loaded and the document is ready to be manipulated. Use the event's callback function to target the paragraph element with id="text" and replace the text with "This is really cool!"
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    changeDom()
+});
+
+function changeDom() {
+    document.getElementById("text")
+        .textContent = "This is really cool!";
+}
+
